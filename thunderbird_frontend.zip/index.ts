8c445bf044897e1d1b996ce85ecc0eac785ea601
sherr <sherr4306@gmail.com>
2025-07-31 15:37:33 +0530

    Delete thunderbird_frontend.zip directory

--- thunderbird_frontend.zip/index.ts
+++
@@ -1,5 +0,0 @@
-export { ChatArea } from "./chat-area";
-export { ChatInput } from "./chat-input";
-export { MessageBubble } from "./message-bubble";
-export { Sidebar } from "./sidebar";
-export { TypingIndicator } from "./typing-indicator";
